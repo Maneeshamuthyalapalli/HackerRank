5e0ac0a7670e5af225fe6df23170cd93b856fc87
Maneeshamuthyalapalli <160912657+Maneeshamuthyalapalli@users.noreply.github.com>
2024-02-24 00:24:05 +0530

    Create SimpleArraySum.c

---
+++ SimpleArraySum.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int n;
+    scanf("%d" ,&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        ans = ans + arr[i];
+    }
+    printf("%d\n", ans);
+    return 0;
+}
